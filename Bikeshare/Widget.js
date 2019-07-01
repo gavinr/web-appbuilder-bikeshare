6e087c58d3fd51e76313a5b6827c8c679359f0c9
Gavin Rehkemper <grehkemper@esri.com>
2019-07-01 09:16:33 -0500

    UI file (Widget.html)

--- Bikeshare/Widget.js
+++ Bikeshare/Widget.js
@@ -13,15 +13,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 ///////////////////////////////////////////////////////////////////////////
-define(['dojo/_base/declare',
-  'jimu/BaseWidget'
-],
-function(declare, BaseWidget) {
+define(["dojo/_base/declare", "jimu/BaseWidget"], function(
+  declare,
+  BaseWidget
+) {
   var clazz = declare([BaseWidget], {
-    templateString: '<div>This is a very simple widget. ' +
-    '<input type="button" value="Get Map Id" data-dojo-attach-event="click:_getMapId">.</div>',
-
-    _getMapId: function(){
+    _getMapId: function() {
       alert(this.map.id);
     }
   });
